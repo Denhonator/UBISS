15baf3e17aa9c1306513a616cb02ecfe7808b3c0
flciprian <46439274+flciprian@users.noreply.github.com>
2019-06-11 18:08:05 +0300

    Basic shooter

--- Assets/Scripts/ShooterScript.cs
+++ Assets/Scripts/ShooterScript.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class ShooterScript : MonoBehaviour
 {
+    public GameObject sphere;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //InvokeRepeating("Shoot", 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        //sphere.transform.position = new Vector3(10, 1, 5);
+        Rigidbody rb = sphere.GetComponent<Rigidbody>();
+        rb.AddForce(-50, 0, 0, ForceMode.Impulse);
     }
 }
