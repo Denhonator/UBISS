55af1b93d015fa3804d511d6cbdd30c2822d4c92
Pauli Sakaria Pouke <32382130+ppouke@users.noreply.github.com>
2019-06-12 17:42:30 +0300

    Cloud vr billboard

---
+++ Assets/Scripts/vr_billboard.cs
@@ -0,0 +1,20 @@
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+     
+    public class vr_billboard : MonoBehaviour
+    {
+        static Transform tCam = null;
+        void Update ()
+        {
+            if(!tCam)
+            {
+                if(!Camera.main)
+                {
+                    return;
+                }
+                tCam = Camera.main.transform;
+            }
+            transform.LookAt(tCam.position, Vector3.up);
+        }
+    }
