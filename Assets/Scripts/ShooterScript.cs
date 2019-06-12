78b45d8cad91e9dc8e55c4b7e9494286c375e670
flciprian <46439274+flciprian@users.noreply.github.com>
2019-06-12 15:37:35 +0300

    Update

--- Assets/Scripts/ShooterScript.cs
+++ Assets/Scripts/ShooterScript.cs
@@ -6,27 +6,30 @@ public class ShooterScript : MonoBehaviour
 {
     public GameObject sphere;
     public Transform playerCamera;
-    float strength = 0.7f;
+    public float strength = 0.7f;
+    public float delay = 3.5f;
     float timer = 0;
 
-    void Update(){
-        if (timer>3.5f)
+    void Update()
+    {
+        if (timer > delay)
         {
             StartCoroutine(Shoot());
             timer = 0;
         }
-        timer+=Time.deltaTime;
+        timer += Time.deltaTime;
     }
 
-    IEnumerator Shoot(){
+    IEnumerator Shoot()
+    {
         sphere.transform.position = transform.position;
         Rigidbody rb = sphere.GetComponent<Rigidbody>();
         rb.velocity = Vector3.zero;
         yield return new WaitForSeconds(0.5f);
-        Vector3 toPlayer = (playerCamera.position-transform.position);
-        toPlayer.x += Random.Range(-5.0f,5.0f);
+        Vector3 toPlayer = (playerCamera.position - transform.position);
+        toPlayer.x += Random.Range(-5.0f, 5.0f);
         toPlayer.y += 10.0f;
-        toPlayer.z += Random.Range(-5.0f,5.0f);
-        rb.AddForce(toPlayer*strength, ForceMode.Impulse);
+        toPlayer.z += Random.Range(-5.0f, 5.0f);
+        rb.AddForce(toPlayer * strength, ForceMode.Impulse);
     }
 }
