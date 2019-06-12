4d8b82ee93203ad5094e9df57e070b3f5bfbb57c
flciprian <46439274+flciprian@users.noreply.github.com>
2019-06-12 18:06:40 +0300

    a

---
+++ Assets/Scripts/CameraFadeImageTry.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraFadeImageTry : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Fade()
+    {
+        Transform image = GameObject.Find("images").transform.Find("cropped-favicon");
+        image.gameObject.SetActive(true);
+        gameObject.GetComponent<SpriteRenderer>().DOFade(0, 1);
+    }
+}
