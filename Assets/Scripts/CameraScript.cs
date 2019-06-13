4b2706fe808b199442243b5ee263a1140f7f974c
flciprian <46439274+flciprian@users.noreply.github.com>
2019-06-13 13:55:06 +0300

    Revert "fade image"
    This reverts commit 0b75ab031836adeb74cb0c9f661d97b18b04130a.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -7,49 +7,40 @@ public class CameraScript : MonoBehaviour
     public int duration = 10;
     public float offset = 0.1f;
 
-    private void OnTriggerEnter(Collider other)
-    {
+    private void OnTriggerEnter(Collider other) {
         Vector3 vel = other.transform.GetComponent<Rigidbody>().velocity;
-        other.transform.GetComponent<Rigidbody>().velocity = new Vector3(-vel.x, vel.y, -vel.z);
+        other.transform.GetComponent<Rigidbody>().velocity = new Vector3(-vel.x,vel.y,-vel.z);
         Debug.Log(vel.sqrMagnitude);
-        if (vel.sqrMagnitude < 10.0f)
+        if(vel.sqrMagnitude<10.0f)
             return;
         StartCoroutine(Shake((transform.parent.position - other.transform.position).normalized));
-        StartCoroutine(GetComponent<CameraFadeImageTry>().Fade());
     }
-    private void OnCollisionEnter(Collision other)
-    {
+    private void OnCollisionEnter(Collision other) {
         Vector3 vel = other.transform.GetComponent<Rigidbody>().velocity;
-        other.transform.GetComponent<Rigidbody>().velocity = new Vector3(-vel.x, vel.y, -vel.z);
+        other.transform.GetComponent<Rigidbody>().velocity = new Vector3(-vel.x,vel.y,-vel.z);
         Debug.Log(vel.sqrMagnitude);
-        if (vel.sqrMagnitude < 0.2f)
+        if(vel.sqrMagnitude<0.2f)
             return;
         StartCoroutine(Shake((transform.parent.position - other.transform.position).normalized));
-        StartCoroutine(GetComponent<CameraFadeImageTry>().Fade());
     }
 
-    private void Start()
-    {
+    private void Start() {
         StartCoroutine(StartUp());
     }
 
-    IEnumerator StartUp()
-    {
+    IEnumerator StartUp() {
         yield return new WaitForSeconds(1.5f);
-        transform.parent.parent.rotation = Quaternion.Euler(0, -transform.rotation.eulerAngles.y, 0);
-        transform.parent.parent.position -= new Vector3(transform.position.x, 0, transform.position.z);
+        transform.parent.parent.rotation = Quaternion.Euler(0,-transform.rotation.eulerAngles.y,0);
+        transform.parent.parent.position -= new Vector3(transform.position.x,0,transform.position.z);
     }
 
-    IEnumerator Shake(Vector3 dir)
-    {
-        for (int i = 1; i <= duration; i++)
-        {
-            transform.parent.localPosition = dir * offset * i;
+    IEnumerator Shake(Vector3 dir){
+        for(int i = 1; i<=duration; i++){
+            transform.parent.localPosition = dir*offset*i;
             yield return new WaitForEndOfFrame();
         }
-        for (int i = duration - 1; i >= 0; i--)
-        {
-            transform.parent.localPosition = dir * offset * i;
+        for(int i = duration-1; i>=0; i--){
+            transform.parent.localPosition = dir*offset*i;
             yield return new WaitForEndOfFrame();
         }
     }
