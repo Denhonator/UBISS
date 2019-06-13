3fcd4ffb836ae397ec611766a8a79715462b4b1c
Pauli Sakaria Pouke <32382130+ppouke@users.noreply.github.com>
2019-06-13 18:15:28 +0300

    Ballon sway script

---
+++ Assets/Scripts/BalloonSway.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonSway : MonoBehaviour
+
+{
+    Vector3 startPos = new Vector3(0, 0, 0);
+    float swayoffset = 0;
+    public float swaySpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+       startPos = transform.position;
+       swayoffset = Random.Range(0, 6);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startPos + new Vector3(Mathf.Sin(Time.deltaTime + swayoffset),0, Mathf.Sin(Time.deltaTime + swayoffset));
+        
+    }
+}
