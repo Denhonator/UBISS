2c6a16317bf44b1e0b7a6afb293f457d96db6d42
Pauli Sakaria Pouke <32382130+ppouke@users.noreply.github.com>
2019-06-13 18:40:11 +0300

    BalloonSway

--- Assets/Scripts/BalloonSway.cs
+++ Assets/Scripts/BalloonSway.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class BalloonSway : MonoBehaviour
 
 {
-    Vector3 startPos = new Vector3(0, 0, 0);
+    Quaternion startRot = Quaternion.Euler(0, 0, 0);
     float swayoffset = 0;
     public float swaySpeed = 1;
+    public float swayAmount;
     // Start is called before the first frame update
     void Start()
     {
-       startPos = transform.position;
+       startRot = transform.rotation;
        swayoffset = Random.Range(0, 6);
         
     }
@@ -19,7 +20,7 @@ public class BalloonSway : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = startPos + new Vector3(Mathf.Sin(Time.deltaTime + swayoffset),0, Mathf.Sin(Time.deltaTime + swayoffset));
+        transform.localRotation = startRot * Quaternion.Euler(Mathf.Sin(swaySpeed * Time.time + swayoffset) * swayAmount, 0, Mathf.Sin(swaySpeed * Time.time + swayoffset) * swayAmount);
         
     }
 }
